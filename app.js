e546402d518189355ea4057eebaed89e5a05d62a
Sateesh Peetha <sateesh_peetha@yahoo.com>
2017-06-17 23:24:28 +0530

    Update app.js

--- app.js
+++ app.js
@@ -4,6 +4,14 @@ function dropdownCtl($scope, $element, $attrs) {
   
 	 $scope.menusearch = "";
 	 $scope.configmenu = config;
+	
+	$scope.toggle = function() {
+		
+		value = document.getElementById("dropdownmenu").style.display;
+		
+		if ( value == "block") {document.getElementById("dropdownmenu").style.display = "none"; }
+		else {document.getElementById("dropdownmenu").style.display = "block"; }
+	}
    
    }
    
